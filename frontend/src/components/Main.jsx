d2e1d8aed745b065dc1d163175b8269e283c6b38
PaoGall112 <shrimpystew889@gmail.com>
2024-08-22 21:04:59 +0800

    changes again in index and main jsx

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -6,51 +6,46 @@ import Experience from './Experience';
 import Projects from './Projects';
 
 function Main() {
-    // const [titleVisible, setTitleVisible] = useState(false);
-    // const [glassesVisible, setGlassesVisible] = useState(false);
-    // const [laptopVisible, setLaptopVisible] = useState(false);
-    // const [serverVisible, setServerVisible] = useState(false);
+    const [titleVisible, setTitleVisible] = useState(false);
+    const [ImgVisible, setImgVisible] = useState(false);
+    const [glassesVisible, setGlassesVisible] = useState(false);
+    const [laptopVisible, setLaptopVisible] = useState(false);
+    const [serverVisible, setServerVisible] = useState(false);
 
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         setTitleVisible(true);
-    //     }, 500);
-    //     setTimeout(() => {                                
-    //         setLaptopVisible(true);
-    //     }, 1500);
-    //     setTimeout(() => {                                
-    //         setServerVisible(true);
-    //     }, 2000);
+    useEffect(() => {
+        setTimeout(() => {
+            setTitleVisible(true);
+        }, 500);
 
-        // const toggleGlasses = () => {
-        //     setTimeout(() => {
-        //         setGlassesVisible(true);
-        //     }, 800);
-        //     setTimeout(() => {                                
-        //         setLaptopVisible(true);
-        //     }, 1500);
-        //     setTimeout(() => {                                
-        //         setServerVisible(true);
-        //     }, 2000);
+        const toggleGlasses = () => {
+            setTimeout(() => {
+                setGlassesVisible(true);
+            }, 800);
+            setTimeout(() => {                                
+                setLaptopVisible(true);
+            }, 1500);
+            setTimeout(() => {                                
+                setServerVisible(true);
+            }, 2000);
 
-        //     setTimeout(() => {
-        //         setGlassesVisible(false); 
-        //     }, 3000);
-        //     setTimeout(() => {                              
-        //         setLaptopVisible(false);
-        //     },4000);
-        //     setTimeout(() => {            
-        //         setServerVisible(false);
-        //     },4300);
-        // };
+            setTimeout(() => {
+                setGlassesVisible(false); 
+            }, 3000);
+            setTimeout(() => {                              
+                setLaptopVisible(false);
+            },4000);
+            setTimeout(() => {            
+                setServerVisible(false);
+            },4300);
+        };
 
-        // toggleGlasses();
+        toggleGlasses();
 
-        // const intervalId = setInterval(toggleGlasses,6000);
+        const intervalId = setInterval(toggleGlasses,6000);
 
-        // return () => clearInterval(intervalId);
+        return () => clearInterval(intervalId);
 
-    // }, []);
+    }, []);
 
     const homePic = require('./assets/home-pic.JPG');
     const glasses = require('./assets/eyeglasses.svg').default;
@@ -65,10 +60,9 @@ function Main() {
                         <div className="container">
                             <div className="home-hero-header">
                                 <div className="home-hero-header-wrap">
-                                    <h1 className={`home-hero-heading text`}>Hello, I'm <span className='home-hero-heading-masked-text'>Paolo!</span></h1> 
-                                    {/* ${titleVisible ? "visible" : ""} */}
-                                    <h2 className={`home-hero-title text`}>Software Developer</h2>
-                                    <div className={`home-hero-subheader text`}>                                     
+                                    <h1 className={`home-hero-heading text ${titleVisible ? "visible" : ""}`}>Hello, I'm <span className='home-hero-heading-masked-text'>Paolo!</span></h1>
+                                    <h2 className={`home-hero-title text ${titleVisible ? "visible" : ""}`}>Software Developer</h2>
+                                    <div className={`home-hero-subheader text ${titleVisible ? "visible" : ""}`}>                                     
                                         <p className="home-hero-subheader-message"> 
                                         I'm a software developer with a passion for front end and back end development.  
                                         I aspire towards a career that will apply my knowledge in developing awesome 
@@ -78,9 +72,9 @@ function Main() {
                                 </div>                                
                                 <div className="home-pic-wrap">
                                     <img src={homePic} alt="menu-icon" className="home-pic" />
-                                    <img src = {glasses} alt = 'glasses' className={`home-pic-glasses`} />
-                                    <img src = {laptop} alt = 'glasses' className={`home-pic-laptop`} />
-                                    <img src = {server} alt = 'glasses' className={`home-pic-server`} />
+                                    <img src = {glasses} alt = 'glasses' className={`home-pic-glasses ${glassesVisible ? "visible" : ""}`} />
+                                    <img src = {laptop} alt = 'glasses' className={`home-pic-laptop ${laptopVisible ? "visible" : ""}`} />
+                                    <img src = {server} alt = 'glasses' className={`home-pic-server ${serverVisible ? "visible" : ""}`} />
                                 </div>
                             </div>
                         </div>
